7d1679af9b77bef54becea7868c4b8ecb6a99be0
KK DD <kk@kk.com>
2018-05-24 23:33:13 +0300

    Chillerlan/qrcode dependecy added

--- src/Controller/QrCodeController.php
+++ src/Controller/QrCodeController.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\QrDatabseManager;
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
 
 class QrCodeController extends Controller
 {
@@ -15,11 +17,27 @@ class QrCodeController extends Controller
      */
     public function index(EntityManagerInterface $em)
     {
+
+        $options = new QROptions([
+            'version'    => 5,
+            'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+            'eccLevel'   => QRCode::ECC_L,
+        ]);
+        
+        // invoke a fresh QRCode instance
+        $qrcode = new QRCode($options);
+        // and dump the output
+        $qrdata='https://google.com';
+        
+
+        //var_dump($qrcode);
+        //die;
         $QrDb=new QrDatabseManager($em);
         return $this->render('qr_code/index.html.twig', [
             'controller_name' => 'QrCodeController',
             'qrCodes' => $QrDb->getQrCodes(),
             'write' => $QrDb->writeQrCode(),
+            'qrcode'=> $qrcode->render($qrdata),
         ]);
     }
 }
